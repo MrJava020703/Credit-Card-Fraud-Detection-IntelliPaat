e87b3c31f9edbcfaa07de23e6d561465b34f300a
Saurabh Kumar (Mr.Java) Web Developer <135533269+MrJava020703@users.noreply.github.com>
2026-01-17 22:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 import tempfile
 import matplotlib.pyplot as plt
 
